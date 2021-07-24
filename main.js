bdc973101c69adc199e7e848c4b61a67f7fbb518
Jerome Flaherty <jeromeflaherty@Jeromes-MacBook-Air.local>
2021-07-23 22:05:16 -0400

    functional push

---
+++ main.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('input');
+const para = document.querySelector('p');
+
+class myArr {
+  constructor(arr) {
+    this.arr = arr;
+    this.new = []
+  }
+
+  scramble() {
+    for(;this.arr.length > 0;){
+      const rand = Math.floor(Math.random() * this.arr.length);
+      const temp = this.arr.splice(rand, 1);
+      this.new.push(temp)
+    }
+    return this.new.join('');
+  }
+}
+
+const handleChange = e => {
+  e.preventDefault();
+  let newArr = new myArr(e.target.value.split(''));
+  const scrambled = newArr.scramble();
+  para.innerText = scrambled;
+}
+
+input.addEventListener('input', handleChange)
